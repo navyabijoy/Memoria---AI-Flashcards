a5d2d9dc0d481ac834751efc82ad32b88360fcb3
Navya Bijoy <navyabijoy14@gmail.com>
2024-08-17 16:01:45 +0530

    added vercel analytics

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react"
 import {
   Grid,
   Button,
