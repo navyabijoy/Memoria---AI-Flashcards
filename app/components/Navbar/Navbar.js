acd8492be51be0466095c64d063af40eda4cb978
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-09-07 19:42:46 +0530

    Update Navbar.js

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -32,7 +32,6 @@ const Navbar = () => {
             </SignedIn>
           </div>
           <div className="sm:hidden flex items-center">
-            {/* Mobile menu button */}
             <button
               type="button"
               onClick={toggleMenu}
@@ -41,11 +40,9 @@ const Navbar = () => {
               aria-expanded={isMenuOpen ? "true" : "false"}
             >
               <span className="sr-only">Open main menu</span>
-              {/* Icon when menu is closed */}
               <svg className={`${isMenuOpen ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
-              {/* Icon when menu is open */}
               <svg className={`${isMenuOpen ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
@@ -54,7 +51,6 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Mobile menu, show/hide based on menu state */}
       {isMenuOpen && (
         <div className="sm:hidden" id="mobile-menu">
           <div className="px-2 pt-2 pb-3 space-y-1">
